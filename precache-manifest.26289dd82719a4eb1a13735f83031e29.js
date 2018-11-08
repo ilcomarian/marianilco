d32fbf5c51294705cf87a2f15afe37ab63e400d1
ilcomarian <ilcomarian@gmail.com>
2018-11-08 13:15:50 -0500

    Updates

---
+++ precache-manifest.26289dd82719a4eb1a13735f83031e29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b9c4b52c6735ff95bc9",
+    "url": "/marianilco/static/js/runtime~main.5b9c4b52.js"
+  },
+  {
+    "revision": "25f6c1b3fc1542a27b9e",
+    "url": "/marianilco/static/js/main.25f6c1b3.chunk.js"
+  },
+  {
+    "revision": "b029c33548e65176f110",
+    "url": "/marianilco/static/js/1.b029c335.chunk.js"
+  },
+  {
+    "revision": "25f6c1b3fc1542a27b9e",
+    "url": "/marianilco/static/css/main.afc0afd4.chunk.css"
+  },
+  {
+    "revision": "92e0438a1bd9c56497eb6af75186d056",
+    "url": "/marianilco/index.html"
+  }
+];
